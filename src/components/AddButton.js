766a525c77a9d1013a206d815c1eddde35db5043
Simone Westphal <sw@simonewestphal.de>
2021-01-10 18:25:14 +0100

    feature: Add Form for new list or card with close button and onblur callback

--- src/components/AddButton.js
+++ src/components/AddButton.js
@@ -1,16 +1,52 @@
 import React from 'react';
 
+const styles = {
+    textarea: {
+        resize: 'none',
+        width: 'calc(100% - 6px)',
+        outline: 'none',
+        minHeight: '100px',
+        padding: '5px',
+    },
+};
 class AddButton extends React.Component {
-    state = { formOpen: false };
-
-    toggleForm = () => {
-        this.setState({ formOpen: !this.state.formOpen });
+    state = {
+        formOpen: false,
+        text: '',
     };
+
+    handleInputChange = (e) => {
+        this.setState({ text: e.target.value });
+    };
+
+    closeForm = () => {
+        this.setState({ formOpen: false });
+    };
+
+    openForm = () => {
+        this.setState({ formOpen: true });
+    };
+
     renderForm = () => {
+        const { list } = this.props;
+
+        const placeholder = list
+            ? 'Enter title for your new list'
+            : 'Enter content for new card';
+        const buttonText = list ? 'Add List' : 'Add Card';
+        //@todo autosize textarea
         return (
             <div>
-                <span onClick={this.toggleForm}>- close form</span>
-                <p>hello i'm the form</p>
+                <p onClick={this.closeForm}>- close form</p>
+                <textarea
+                    style={styles.textarea}
+                    placeholder={placeholder}
+                    autoFocus
+                    onBlur={this.closeForm}
+                    value={this.state.text}
+                    onChange={this.handleInputChange}
+                />
+                {this.state.formOpen ? <button>{buttonText}</button> : ''}
             </div>
         );
     };
@@ -18,8 +54,9 @@ class AddButton extends React.Component {
     renderAddButton = () => {
         const { list } = this.props;
         const buttonText = list ? 'Add another list' : 'Add another card';
-        return <div onClick={this.toggleForm}>+ Add another {buttonText}</div>;
+        return <div onClick={this.openForm}>+ Add another {buttonText}</div>;
     };
+
     render() {
         return (
             <div>
