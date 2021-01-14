9eb9bbd41bb04d66efe76cc4fe647f8774ed29df
Simone Westphal <hello@swestphal.io>
2021-01-14 19:22:26 +0100

    refactor: Change App to functional component

--- src/components/App.js
+++ src/components/App.js
@@ -4,19 +4,20 @@ import { connect } from 'react-redux';
 import AddButton from './AddButton';
 import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 import { sort } from '../actions';
+
 const styles = {
     board: {
         display: 'flex',
     },
 };
-class App extends Component {
-    onDragEnd = (result) => {
+const App = (props) => {
+    const onDragEnd = (result) => {
         const { destination, source, draggableId, type } = result;
         if (!destination) {
             return;
         }
 
-        this.props.dispatch(
+        props.dispatch(
             sort(
                 source.droppableId,
                 destination.droppableId,
@@ -27,42 +28,42 @@ class App extends Component {
             )
         );
     };
-    render() {
-        const { lists } = this.props;
-        return (
-            <DragDropContext onDragEnd={this.onDragEnd}>
-                <div className='App'>
-                    <header className='App-header'>Your boards</header>
-                    <Droppable
-                        droppableId='droppable'
-                        direction='horizontal'
-                        type='list'
-                    >
-                        {(provided) => (
-                            <div
-                                style={styles.board}
-                                {...provided.droppableProps}
-                                ref={provided.innerRef}
-                            >
-                                {lists.map((list, index) => (
-                                    <List
-                                        listID={list.id}
-                                        key={list.id}
-                                        title={list.title}
-                                        cards={list.cards}
-                                        index={index}
-                                    />
-                                ))}
-                                {provided.placeholder}
-                                <AddButton list />
-                            </div>
-                        )}
-                    </Droppable>
-                </div>
-            </DragDropContext>
-        );
-    }
-}
+
+    const { lists } = props;
+    console.log(props);
+    return (
+        <DragDropContext onDragEnd={onDragEnd}>
+            <div className='App'>
+                <header className='App-header'>Your boards</header>
+                <Droppable
+                    droppableId='droppable'
+                    direction='horizontal'
+                    type='list'
+                >
+                    {(provided) => (
+                        <div
+                            style={styles.board}
+                            {...provided.droppableProps}
+                            ref={provided.innerRef}
+                        >
+                            {lists.map((list, index) => (
+                                <List
+                                    listID={list.id}
+                                    key={list.id}
+                                    title={list.title}
+                                    cards={list.cards}
+                                    index={index}
+                                />
+                            ))}
+                            {provided.placeholder}
+                            <AddButton list />
+                        </div>
+                    )}
+                </Droppable>
+            </div>
+        </DragDropContext>
+    );
+};
 
 const mapStateToProps = (state) => ({
     lists: state.lists,
