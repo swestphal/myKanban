ef8db7f7d8a04e19c6d4528c5450d064c5e89ffc
Simone Westphal <hello@swestphal.io>
2021-01-23 14:31:47 +0100

    refactor: Build vlient/server structure and move files

--- src/actions/index.js
+++
@@ -1,8 +0,0 @@
-export * from './listsAction';
-export * from './cardsAction';
-
-export const CONSTANTS = {
-    ADD_CARD: 'ADD_CARD',
-    ADD_LIST: 'ADD_LIST',
-    DRAG_FINISHED: 'DRAG_FINISHED',
-};
