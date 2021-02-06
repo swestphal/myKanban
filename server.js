df3e870d55474c00d3b703a2c9e63c1a2f4bc2af
Simone Westphal <hello@swestphal.io>
2021-02-06 09:44:43 +0100

    refactor: Initial config for heroku

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db')
+const path = require('path')
+
 const app = express();
 
 connectDB();
@@ -7,7 +9,7 @@ connectDB();
 // init middleware
 app.use(express.json({ extended: false }))
 
-app.get('/', (req, res) => res.send('API is running'));
+// app.get('/', (req, res) => res.send('API is running'));
 
 // define routes
 
@@ -17,7 +19,17 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/lists', require('./routes/api/lists'));
 
+// serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+
+    // set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT} `));
