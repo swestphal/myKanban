a0f9928f8c60c1946b54c39feb8725dfeb2ff4be
Simone Westphal <hello@swestphal.io>
2021-02-10 18:04:32 +0100

    feature: Add cards route to api

--- routes/api/cards.js
+++ routes/api/cards.js
@@ -1,7 +1,41 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+const request = require('request');
+
+
+const Card = require('../../models/Card');
+
 // @route   POST api/cards
 // @desc    Create a card
 // @access  Private
+router.post(
+    '/',
+    auth,
+    async (req, res) => {
+        console.log("newCard")
+        console.log(req.body)
+        try {
+            const listID = req.body.listID.split('-')[1]
+            const newCard = new Card({
+                _list: listID,
+                card_title: req.body.formData.title,
+                card_text: req.body.formData.text,
 
+            });
+
+            console.log(newCard)
+
+            const card = await newCard.save();
+
+            res.json(card);
+        } catch (err) {
+
+            res.status(500).send('Server Error');
+        }
+    }
+);
 
 
 // @route   Get api/cards
@@ -19,3 +53,6 @@
 // @route   DELETE api/cards/:id
 // @desc    Delete single card
 // @access  Private
+
+
+module.exports = router;
